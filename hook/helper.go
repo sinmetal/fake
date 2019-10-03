9fe09d6275f4798e75c0498d37b13f7a20b72a4b
sinmetal <metal.tie@gmail.com>
2019-10-03 20:46:15 +0900

    地味に差が出るので外した

--- hook/helper.go
+++ hook/helper.go
@@ -16,7 +16,7 @@ var Update = flag.Bool("update", false, "update golden file")
 var Generate = flag.Bool("gen", false, "code generate")
 
 func IgnoreHeaderKey(key string) bool {
-	keys := []string{"Expires", "Age", "X-GUploader-UploadID", "Alt-Svc", "Date"}
+	keys := []string{"Expires", "Age", "X-GUploader-UploadID", "Alt-Svc", "Date", "X-Goog-Api-Client"}
 	for _, v := range keys {
 		if strings.ToLower(key) == strings.ToLower(v) {
 			return true
